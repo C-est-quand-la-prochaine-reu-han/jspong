07655fb22ac9dc375474fb24b11119f466204a9d
Ratsock <ratsockfofox@gmail.com>
2024-09-23 18:32:27 +0200

    Add function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-token = "0ca7498892b9d855a206bd5d4dda60133bb65a27"
+token = "9e81fbfd3c32255e0393b64ecd6f76e5267a0262"
 path = "http://localhost:8000/appong/api/"
 debug = true
 
@@ -17,7 +17,7 @@ async function get() {
 }
 
 async function get_all_matches() {
-    let response = await fetch(path + 'match', { headers : headers });
+    let response = await fetch(path + 'match/', { headers : headers });
     let data = await response.json();
     if (debug)
         console.log(data);
@@ -25,7 +25,7 @@ async function get_all_matches() {
 }
 
 async function get_all_users() {
-    let response = await fetch(path + 'user', { headers : headers });
+    let response = await fetch(path + 'user/', { headers : headers });
     let data = await response.json();
     if (debug)
         console.log(data);
@@ -33,7 +33,7 @@ async function get_all_users() {
 }
 
 async function get_all_tournament() {
-    let response = await fetch(path + 'tournament', { headers : headers });
+    let response = await fetch(path + 'tournament/', { headers : headers });
     let data = await response.json();
     if (debug)
         console.log(data);
@@ -41,7 +41,7 @@ async function get_all_tournament() {
 }
 
 async function get_number_of_matches() {
-    let response = await fetch(path + 'match', { headers : headers });
+    let response = get_all_matches();
     let data = await response.json();
     if (debug)
         console.log(data.length);
@@ -49,7 +49,7 @@ async function get_number_of_matches() {
 }
 
 async function get_number_of_users() {
-    let response = await fetch(path + 'user', { headers : headers });
+    let response = await fetch(path + 'user/', { headers : headers });
     let data = await response.json();
     if (debug)
         console.log(data.length);
@@ -57,7 +57,7 @@ async function get_number_of_users() {
 }
 
 async function get_number_of_tournaments() {
-    let response = await fetch(path + 'tournament', { headers : headers });
+    let response = await fetch(path + 'tournament/', { headers : headers });
     let data = await response.json();
     if (debug)
         console.log(data.length);
@@ -194,9 +194,115 @@ async function get_all_matches_of_user(id) {
 }
 
 
+async function get_username_by_id(id) {
+    let user = await get_user_by_id(id);
+    if (debug)
+        console.log(user.user.username);
+    return user.user.username;
+}
+
+async function get_nickname_by_id(id) {
+    let user = await get_user_by_id(id);
+    if (debug)
+        console.log(user.user.username);
+    return user.user.user_nick;
+}
+
+async function create_user(username, password) {
+    let data = {
+        "user": {
+            "username": username,
+            "password": password
+        },
+        "user_nick": username
+    };    
+    let response = await fetch(path + 'user/', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+    let user = await response.json();
+    if (debug)
+        console.log(user);
+    return user;
+}
+
+async function update_user_nickname(id, nickname) {
+    let data = {
+        "user_nick": nickname
+    };    
+    let response = await fetch(path + 'user/' + id + '/', {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+    let user = await response.json();
+    if (debug)
+        console.log(user);
+    return user;
+}
 
 
+// TODO: Verifier que cela fonctionne
+async function update_user_avatar(id, avatar) {
+    if (avatar)
+    {
+        const formData = new FormData();
+        formData.append('avatar', file);
+        let response = await fetch(path + 'user/' + id + '/', {
+            method: 'PUT',
+            headers: headers,
+            body: avatar
+        });
+        let user = await response.json();
+        if (debug)
+            console.log(user);
+    }
+    return user;
+}
 
+async function create_tournament(name, pending) {
+    let data = {
+        "name": name,
+        "pending": pending
+    };    
+    let response = await fetch(path + 'tournament/', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+    let tournament = await response.json();
+    if (debug)
+        console.log(tournament);
+    return tournament;
+}
 
+async function confirm_tournament(id) {
+    let response = await fetch(path + 'tournament/' + id + '/confirm/', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+    let tournament = await response.json();
+    if (debug)
+        console.log(tournament);
+    return tournament;
+}
 
+async function create_match(player1, player2, tournament) {
+    let data = {
+        "tournament": tournament,
+        "player1": player1,
+        "player2": player2,
+    };
+    let response = await fetch(path + 'match/', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+    let match = await response.json();
+    if (debug)
+        console.log(match);
+    return match;
+}
 
